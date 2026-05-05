de56cd6a10c6d283e082f9cf65c65a24dd38ecab
Chinonso <ceothedev18@gmail.com>
2026-05-05 10:11:50 +0100

    Added Vercel.json and updated vite config

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,13 @@ export default defineConfig({
   },
   build: {
     target: "esnext",
+    outDir: "dist", // Vercel will serve from here
+  },
+  server: {
+    headers: {
+      // Required for fhevmjs WASM + SharedArrayBuffer
+      "Cross-Origin-Opener-Policy": "same-origin",
+      "Cross-Origin-Embedder-Policy": "require-corp",
+    },
   },
 });
